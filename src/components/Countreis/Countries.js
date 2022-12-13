6b2172590b9ac930bdc95f8375cc08185056ab47
niloyabdullah <abdullahniloy149@gmail.com>
2022-12-13 15:19:19 +0600

    unique key use

--- src/components/Countreis/Countries.js
+++ src/components/Countreis/Countries.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import Country from './../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -11,25 +12,30 @@ const Countries = () => {
 
     }, [])
     return (
-        <div>
+
+        <div >
             <h1>Load Countries Data</h1>
             <h2>Available Countries:{countries.length}</h2>
-            {
-                countries.map(country => console.log(country))
-            }
+            <div className='countriesGrid'>
+                {/* {
+                    countries.map(country => console.log(country))
+                } */}
 
-            {
-                countries.map(country => <Country
-                    country={country}
-                //name={country.name.common}
-                // population={country.population}
-                // flags={country.flags.png}
-                // area={country.area}
-                // capital={country.capital}
-                ></Country>)
-                /*ei vabe ekta ekta kore na pathiye multiple data pass kora jay. mane jodi 'country' kei pathiye dei tahole map korar somoy Attribute hisebe data lagbe na. just props theke data ber kore nilei hbe.*/
-            }
-        </div>
+                {
+                    countries.map(country => <Country
+                        country={country}
+                        key={country.cca3}
+                    //name={country.name.common}
+                    // population={country.population}
+                    // flags={country.flags.png}
+                    // area={country.area}
+                    // capital={country.capital}
+                    ></Country>)
+                    /*ei vabe ekta ekta kore na pathiye multiple data pass kora jay. mane jodi 'country' kei pathiye dei tahole map korar somoy Attribute hisebe data lagbe na. just props theke data ber kore nilei hbe.*/
+                }
+            </div>
+
+        </div >
     );
 };
 
